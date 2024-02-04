008ef74f2f1b9d1c5078f28a7025cf1cb43cf249
Kirill <kirillbronski@gmail.com>
2024-02-04 21:11:58 +0300

    07 state p1

---
+++ app/src/main/java/com/kbcoding/androidcomposebasics/CounterState.kt
@@ -0,0 +1,5 @@
+package com.kbcoding.androidcomposebasics
+
+data class CounterState(
+    val count: Int = 0
+)
--- app/src/main/java/com/kbcoding/androidcomposebasics/MainActivity.kt
+++ app/src/main/java/com/kbcoding/androidcomposebasics/MainActivity.kt
@@ -6,30 +6,66 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.kbcoding.androidcomposebasics.ui.theme.AndroidComposeBasicsTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            StatefulCounter()
         }
     }
 }
 
+@Composable
+fun StatefulCounter() {
 
+    // v1
+    val counterMutableState = remember {
+        mutableStateOf(CounterState(Random.nextInt(1000)))
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = counterMutableState.value.count.toString(),
+            fontSize = 60.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(onClick = {
+            val state = counterMutableState.value
+            val newState = state.copy(count = state.count + 1)
+            counterMutableState.value = newState
+        }
+        ) {
+            Text(text = "Increment")
+        }
+    }
+}
 
 @Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-
+fun Preview() {
+    StatefulCounter()
 }
