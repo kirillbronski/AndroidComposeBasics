544787127350cd136722eada07dce651db82b8e0
Kirill <kirillbronski@gmail.com>
2024-02-11 23:41:58 +0300

    L27 BackHandler

---
+++ l27-backhandler/src/main/java/com/kbcoding/l27_backhandler/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.kbcoding.l27_backhandler
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppScreen()
+        }
+    }
+
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun AppScreen() {
+    var counter by remember {
+        mutableIntStateOf(0)
+    }
+
+    BackHandler {
+        println("BackHandlerTest 111")
+    }
+
+    if (counter % 2 == 0) {
+        BackHandler {
+            println("BackHandlerTest 222")
+        }
+        Box(modifier = Modifier.size(60.dp).background(Color.Red))
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Text(
+            text = counter.toString(),
+            fontSize = 60.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace,
+        )
+        Spacer(modifier = Modifier.height(100.dp))
+        Button(
+            onClick = { counter++ }
+        ) {
+            Text(
+                text = stringResource(R.string.increment),
+                fontSize = 18.sp,
+            )
+        }
+    }
+}
