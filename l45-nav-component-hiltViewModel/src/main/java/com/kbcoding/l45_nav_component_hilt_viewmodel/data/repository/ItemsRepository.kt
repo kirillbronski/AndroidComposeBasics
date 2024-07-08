14f0b220f278f9e357d887db7a58618d53c4691b
Kirill <kirillbronski@gmail.com>
2024-07-08 23:23:30 +0300

    l45 navigation component: hilt view model - finish project

--- l45-nav-component-hiltViewModel/src/main/java/com/kbcoding/l45_nav_component_hilt_viewmodel/data/repository/ItemsRepository.kt
+++ l45-nav-component-hiltViewModel/src/main/java/com/kbcoding/l45_nav_component_hilt_viewmodel/data/repository/ItemsRepository.kt
@@ -2,6 +2,11 @@ package com.kbcoding.l45_nav_component_hilt_viewmodel.data.repository
 
 import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -9,4 +14,16 @@ import javax.inject.Singleton
 class ItemsRepository @Inject constructor(
     @ApplicationContext private val context: Context,
 ) {
+
+    private val itemsFlow =
+        MutableStateFlow(List(size = 10) { "Item ${it + 1}" })
+
+    suspend fun addItem(title: String) {
+        delay(2000)
+        itemsFlow.update { it + title }
+    }
+
+    fun getItems(): Flow<List<String>> {
+        return itemsFlow.onStart { delay(3000) }
+    }
 }
